ada3c2f27ceafffd8292d9821ee52d5264175c51
Hortobágyi Róbert <hortobagyi.robert@gmail.com>
2023-04-22 10:03:58 +0200

    added basic functionality

--- Webfejlesztés/urlap.js
+++ Webfejlesztés/urlap.js
@@ -1,9 +1,31 @@
 const form = document.querySelector('form');
 form.addEventListener('submit', submitForm);
 
+function oracleTransform(text) {
+  const splitText = text.split(' ')
+  console.log(splitText);
+  let escapedOracle = "%28"
+
+  for (const element of splitText) {
+    escapedOracle = escapedOracle + "oracle%3A" + element + "+"
+  }
+
+  escapedOracle = escapedOracle + "%29"
+  console.log(escapedOracle)
+  
+
+}
+
 function submitForm(event) {
-  event.preventDefault();
+  /*event.preventDefault();
   const nameInput = document.getElementById("name");
+  const textInput = document.getElementById("box-text");
   const name = nameInput.value;
-  console.log(name);
+  const oracleText = textInput.value;
+  let searchUrl = "https://scryfall.com/search?q="+name+"+%28oracle%3A"+ oracleText + "%29&unique=cards&as=grid&order=name"
+  window.open(searchUrl);*/
+  const textInput = document.getElementById("box-text");
+  const oracleText = textInput.value;
+  oracleTransform("Ferenc asd")
+  return false
 }
